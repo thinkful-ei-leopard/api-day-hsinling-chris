bb7ed61ca8ee73f7e6c2eca9c78f9184cef29d32
Christopher Floyd <cfloyd252@gmail.com>
2020-01-16 12:37:01 -0700

    completed 4

--- src/api.js
+++ src/api.js
@@ -2,9 +2,20 @@ const BASE_URL = 'https://thinkful-list-api.herokuapp.com/chris';
 
 function getItems(){
   return  fetch(`${BASE_URL}/items`);
-    
+}
+
+function createItem(name){
+    let newItem = {
+        name: name
+    };
+    newItem = JSON.stringify(newItem);
+
+    return fetch(`${BASE_URL}/items`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }, body: newItem})
 }
 
 export default {
-    getItems
+    getItems,
+    createItem
 };
