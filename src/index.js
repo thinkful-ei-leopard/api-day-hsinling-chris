ab98e6bbe23cdf6e797eb7b8dea4ff37a3fa18ef
Hsin Ling Hu <hsinkava11@gmail.com>
2020-01-16 12:00:25 -0800

    exercise 5

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import './index.css';
 import api from './api';
 
 import shoppingList from './shopping-list';
+import store from './store';
 
 const main = function () {
-
+  api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
+
+
 
 $(main);
